d58c58767b76fc60873883987f49b4f55db9c4f1
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-05-04 15:16:22 +0530

    Add props.theme types

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,8 +8,16 @@ export const Theme = {
     brightTurquoise: '#03DFD8',
     white: '#FFFFFF',
     gallery: '#EBEBEB',
+    pacificBlue: '#009AC9',
+    nepal: '#899DBF',
   }
 };
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: typeof Theme.colors;
+  }
+}
 
 export type Theme = typeof Theme;
 export const styled = baseStyled as ThemedStyledInterface<Theme>;
