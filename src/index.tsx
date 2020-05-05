87d01331a0339cdf657c739df74c1588d9157ad0
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-05-05 15:12:09 +0530

    feature: Add logo & basic animation [app-shell] (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, keyframes} from 'styled-components';
 
 import StoreProvider from '@contexts/Store';
+
+// styling
 import GlobalStyles from '@styles/globalStyles';
 import { Theme } from '@styles/theme';
-import PlayArea from './components/PlayArea';
-import Controls from './components/Controls';
 
-const App = () => (
-  <>
-    <ThemeProvider
-      theme={Theme}
-    >
-      <GlobalStyles />
-      <StoreProvider>
-        <PlayArea />
-        <Controls/>
-      </StoreProvider>
-    </ThemeProvider>
-  </>
-);
+// components
+import PlayArea from '@components/PlayArea';
+import Controls from '@components/Controls';
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const AppWrapper = styled.div`
+  animation: ${fadeIn} 0.75s;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+`;
+
+const App = () => {
+  return (
+    <AppWrapper>
+      <ThemeProvider
+        theme={Theme}
+      >
+        <GlobalStyles />
+        <StoreProvider>
+          <PlayArea />
+          <Controls/>
+        </StoreProvider>
+      </ThemeProvider>
+    </AppWrapper>
+  )
+};
 
 render(<App />, document.getElementById('app'));
